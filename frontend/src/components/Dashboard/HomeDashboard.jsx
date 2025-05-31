096cef601774495869b31916a6ef528a2ee62943
Mahadi <hassan334469@gmail.com>
2025-05-31 14:20:12 +0600

    Updated shadow

--- frontend/src/components/Dashboard/HomeDashboard.jsx
+++ frontend/src/components/Dashboard/HomeDashboard.jsx
@@ -6,7 +6,7 @@ export default function Dashboard() {
     <div className="flex h-[920px] rounded-3xl bg-[#4e4e4c]">
       <Sidebar />
       <main className="flex-1 p-6">
-        <div className="bg-[#676765] rounded-3xl p-6 shadow-xl h-full">
+        <div className="bg-[#676765] rounded-3xl p-6 shadow-[10px_8px_40px_rgba(0,0,0,0.9)] h-full">
           <h1 className="text-3xl text-white font-bold mb-4">Dashboard</h1>
         </div>
       </main>
