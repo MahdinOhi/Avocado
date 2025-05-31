cb4d2848d5e53ff2c5759d2dd600495b40668d98
Pulok Akibuzzaman <pulok519@gmail.com>
2025-05-31 16:31:19 +0600

    Added SearchBar

---
+++ frontend/src/components/Layout/Searchbar.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import SearchIcon from '@mui/icons-material/Search';
+
+const Searchbar = ({ onSearch }) => {
+    const [query, setQuery] = useState('');
+
+    const handleInputChange = (e) => {
+        setQuery(e.target.value);
+        if (onSearch) {
+            onSearch(e.target.value);
+        }
+    };
+
+    return (
+        <div style={{ marginBottom: '2.25rem', width: '50%', position: 'relative', marginLeft: 'auto', marginRight: 'auto' }}>
+            <div style={{ position: 'absolute', left: '10px', top: '50%', transform: 'translateY(-50%)', color: 'gray' }}>
+                <SearchIcon />
+            </div>
+            <input
+                type="text"
+                value={query}
+                onChange={handleInputChange}
+                placeholder="Search through your notes, tasks, and projects"
+                style={{
+                    width: '100%',
+                    padding: '0.75rem 0.5rem 0.75rem 40px', // left padding makes space for the icon
+                    borderRadius: '12px',
+                    border: '3px solid black',
+                    fontSize: '0.875rem',
+                    outline: 'none',
+                    backgroundColor: 'white'
+                }}
+            />
+        </div>
+    );
+};
+
+export default Searchbar;
